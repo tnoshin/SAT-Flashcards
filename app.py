83f532058d39cc05d8ac11271ab912228b229bd7
tnoshin <tnoshin18@gmail.com>
2026-09-18 23:23:26 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from csv import DictReader
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html', cards=load_cards()) 
+
+
+def load_cards():
+    with open('vocab.csv','r', encoding='utf-8-sig') as file:
+        csv_reader = DictReader(file)
+        flashcards = []
+
+        for r in csv_reader:
+            flashcards.append(r)
+    return flashcards
+
+if __name__=='__main__':
+    app.run(debug=True)
